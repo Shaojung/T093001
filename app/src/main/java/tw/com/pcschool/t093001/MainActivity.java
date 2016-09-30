68673030cf5694a1df8fdec906e4a08cb982f216
Shaojung <abc@abc.com>
2016-09-30 20:13:44 +0800

    Volley Image Request practice.

--- app/src/main/java/tw/com/pcschool/t093001/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t093001/MainActivity.java
@@ -1,12 +1,15 @@
 package tw.com.pcschool.t093001;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -28,7 +31,22 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("NET", error.toString());
             }
         });
+
+        ImageRequest req2 = new ImageRequest("http://www.pcschool.com.tw/updimg/mbrpdt/636104311716853339_B.jpg", new Response.Listener<Bitmap>() {
+            @Override
+            public void onResponse(Bitmap response) {
+                ImageView img = (ImageView) findViewById(R.id.imageView);
+                img.setImageBitmap(response);
+            }
+        }, 0, 0, ImageView.ScaleType.CENTER , Bitmap.Config.RGB_565, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
         queue.add(request);
+        queue.add(req2);
         queue.start();
     }
 }
